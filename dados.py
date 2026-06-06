78d1a660f2f470e70541ab6d207322f0463a5007
igupingu <ferreiraigor3011@gmail.com>
2026-06-06 20:10:20 -0300

    dados

---
+++ dados.py
@@ -0,0 +1,9 @@
+combustiveis = (
+    ("Gasolina Comum", 5.79),
+    ("Gasolina Aditivada", 5.99),
+    ("Etanol", 4.39),
+    ("Diesel S10", 6.09)
+)
+clientes = []
+vendas = []
+cpfs_registrados = set()
